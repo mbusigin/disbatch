mbusigin/disbatch

commit b956a7c1ca0f890cc1d9191eb25ba42be8b22f16

changes to all packages: use 5.12.0 and warnings, some simplification, style changes.
deleted:
- lib/Pinscher/Core/EventBus.pm-sysv
- lib/Pinscher/Core/EventBus.pm-thread
- lib/Synacor/Disbatch/Queue/Enclosure.pm

lib/Pinscher/Core/EventBus.pm:
- removed set_self()

lib/Synacor/Disbatch/ChunkedTaskFactory.pm:
- no use Synacor::Disbatch::Task

lib/Synacor/Disbatch/Engine.pm:

lib/Synacor/Disbatch/HTTP.pm:
- merged worker_thread() into start()

lib/Synacor/Disbatch/Task.pm:
- removed start() and wait_task_status_change()
- made run() unimplemented

lib/Synacor/Disbatch/Timer.pm:
- merged thread_run() into start()

lib/Synacor/Disbatch/WorkerThread.pm:
- no use Synacor::Disbatch::Engine
- merged startstuff() into thread_start()

t/001_base.t:
- removed the test for deleted Synacor::Disbatch::Queue::Enclosure

lib/Synacor/Disbatch/WorkerThread.pm
@@ -1,144 +1,122 @@
 package Synacor::Disbatch::WorkerThread;
 
-use strict;
+use 5.12.0;
+use warnings;
+
+use Carp;
 use Data::Dumper;
 use Pinscher::Core::EventBus;
 use Synacor::Disbatch::Engine;
 use Try::Tiny;
-use Carp;
-use Synacor::Disbatch::Engine;
 
 sub new {
-    my $class = shift;
-    my $id    = shift;
-    my $queue = shift;
+    my ($class, $id, $queue) = @_;
 
-    my $self = {};
+    my $self = {
+        eb        => Pinscher::Core::EventBus->new(bless({}, $class), "worker#$id" ),
+        id        => $id,
+        queue     => $queue,
+        config    => $Synacor::Disbatch::Engine::Engine->{config},
+        data      => {},
+        tasks_run => 0,
+    };
+
+    $self->{eb}{procedures}{start_task} = \&start_task;
+    $self->{eb}{methods}{retire}        = \&retire;
+
     bless $self, $class;
-
-    $self->{'eb'}                             = Pinscher::Core::EventBus->new( $self, 'worker#' . $id );
-    $self->{'id'}                             = $id;
-    $self->{'queue'}                          = $queue;
-    $self->{'eb'}{'procedures'}{'start_task'} = \&start_task;
-    $self->{'eb'}{'methods'}{'retire'}        = \&retire;
-    $self->{'config'}                         = $Synacor::Disbatch::Engine::Engine->{config};
-    $self->{'data'}                           = {};
-    $self->{'tasks_run'}                      = 0;
 
     $self->thread_start;
 
-    return $self;
+    $self;
 }
 
 sub start_task {
-    my $self = shift or confess "No self!";
-    my $task = shift or confess "No task!";
+    my ($self, $task) = @_;
+    confess "No task!" unless $task;
 
-    if ( !$self->{id} ) {
+    if (!$self->{id}) {
         confess "No 'id'!";
         return 1;
     }
 
-    #     print $self->{ 'id' } . ': ' . ref($task) . "\n";
-    $task->{'workerthread'} = $self;
+    #print $self->{ 'id' } . ': ' . ref($task) . "\n";
+    $task->{workerthread} = $self;
 
     try {
         $task->run($self);
-    }
-    catch {
+    } catch {
         $self->logger("Thread has uncaught exception: $_");
-        $Synacor::Disbatch::Engine::EventBus->report_task_done( $task->{'queue_id'}, $task->{'_id'}, 2, 'Unable to complete', "Thread has uncaught exception: $_" );
+        $Synacor::Disbatch::Engine::EventBus->report_task_done($task->{queue_id}, $task->{_id}, 2, 'Unable to complete', "Thread has uncaught exception: $_");
     };
 
-    return 1;
+    1;
 }
 
 sub retire {
-    my $self = shift or confess "No self!";
-    $self->{eb}->{retire} = 1;
-}
-
-sub startstuff {
-    my $self = shift;
-
-    $self->{'eb'}->run;
-    $self->logger->info("This thread $$ has outlived its usefulness");
-    exit(0);
+    my ($self) = @_;
+    $self->{eb}{retire} = 1;
 }
 
 sub thread_start {
-    my $self = shift;
+    my ($self) = @_;
 
-    my $pid = fork();
-    if ( $pid == 0 ) {
-        startstuff($self);
+    defined(my $pid = fork) or die "fork failed: $!";
+    if (!$pid) {
+        $self->{eb}->run;
+        $self->logger->info("This thread $$ has outlived its usefulness");
+        exit 0;
     }
 
-    #    my $ret = threads->create( \&startstuff, $self );
     $self->{pid} = $pid;
     return $pid;
 }
 
 sub kill {
-    my $self = shift;
-    if ( kill 'KILL', $self->{pid} ) {
-        print 'killed ' . __PACKAGE__ . " with PID $self->{pid}\n";
-    }
-    else {
-        print 'could not kill ' . __PACKAGE__ . " with PID $self->{pid}\n";
+    my ($self) = @_;
+    if (kill 'KILL', $self->{pid}) {
+        say 'killed ' . __PACKAGE__ . " with PID $self->{pid}";
+    } else {
+        say 'could not kill ' . __PACKAGE__ . " with PID $self->{pid}";
     }
 }
 
 sub logger {
-    my $self = shift or confess "No self!";
-    my $classname = ref( $self->{queue} );
+    my ($self, $type) = @_;
+    my $classname = ref $self->{queue};
     $classname =~ s/^.*:://;
 
-    my $logger = shift;
-    if ($logger) {
-        my $l = "disbatch.plugins.$classname.$logger";
-        $logger = $l;
-    }
-    else {
-        $logger = "disbatch.engine.$classname";
-    }
+    my $logger = defined $type ? "disbatch.plugins.$classname.$type" : "disbatch.engine.$classname";
 
-    if ( !$self->{log4perl_initialised} ) {
-        Log::Log4perl::init( $self->{config}->{log4perl_conf} );
+    if (!$self->{log4perl_initialised}) {
+        Log::Log4perl::init($self->{config}{log4perl_conf});
         $self->{log4perl_initialised} = 1;
         $self->{loggers}              = {};
     }
 
-    return $self->{loggers}->{$logger} if ( $self->{loggers}->{$logger} );
-    $self->{loggers}->{$logger} = Log::Log4perl->get_logger($logger);
-    return $self->{loggers}->{$logger};
+    $self->{loggers}{$logger} //= Log::Log4perl->get_logger($logger);
+    $self->{loggers}{$logger};
 }
 
 sub mongo {
-    my $self = shift or confess "No self!";
-
-    return $self->{mongo} if defined( $self->{mongo} );
-    $self->{mongo} = Synacor::Disbatch::Backend::connect_mongo( $self->{'config'}->{'mongohost'}, $self->{'config'}->{'mongodb'} );
-    return $self->{mongo};
+    my ($self) = @_;
+    $self->{mongo} //= Synacor::Disbatch::Backend::connect_mongo($self->{config}->{mongohost}, $self->{config}->{mongodb});
+    $self->{mongo};
 }
 
 sub set {
-    my $self = shift or confess "No self!";
-    my $key  = shift or confess "No key!";
-    my $value = shift;
-    $self->{'data'}->{$key} = $value;
+    confess "No value!" unless @_ == 3;
+    $_[0]->{data}{$_[1]} = $_[2];
 }
 
 sub get {
-    my $self = shift or confess "No self!";
-    my $key  = shift or confess "No key!";
-    return $self->{'data'}->{$key};
+    confess "No key!" unless @_ == 2;
+    $_[0]->{data}{$_[1]};
 }
 
 sub unset {
-    my $self = shift or confess "No self!";
-    my $key  = shift or confess "No key!";
-    delete $self->{'data'}->{$key};
+    confess "No key!" unless @_ == 2;
+    delete $_[0]->{data}{$_[1]};
 }
 
 1;

lib/Pinscher/Core/EventBus.pm
@@ -1,15 +1,17 @@
 package Pinscher::Core::EventBus;
 
-use strict;
-use IPC::Shareable (':lock');
+use 5.12.0;
+use warnings;
+
 use AutoLoader;
 use Data::Dumper;
-use Storable qw(nfreeze thaw);
-use IPC::SysV qw(IPC_PRIVATE IPC_RMID IPC_CREAT S_IRWXU);
-use POSIX qw(ceil);
+use File::Temp qw/ tempfile tempdir /;
 use IO::Socket qw(SOCK_STREAM);
 use IO::Socket::UNIX;
-use File::Temp qw/ tempfile tempdir /;
+use IPC::Shareable (':lock');
+use IPC::SysV qw(IPC_PRIVATE IPC_RMID IPC_CREAT S_IRWXU);
+use POSIX qw(ceil);
+use Storable qw(nfreeze thaw);
 use Try::Tiny;
 
 our $AUTOLOAD;
@@ -23,29 +25,25 @@ our %sockets;
 my $MAXQSEND = 80000;
 
 sub new {
-    my $class     = shift;
-    my $self_self = shift;
-    my $name      = shift;
+    my ($class, $self_self, $name) = @_;
 
-    tie %sockets, 'IPC::Shareable', $ipckey1,
-      {
+    tie %sockets, 'IPC::Shareable', $ipckey1, {
         create    => 1,
         exclusive => 0,
         mode      => 0644,
         destroy   => 0,
-      };
+    };
 
-    tie $ebid, 'IPC::Shareable', $ipckey2,
-      {
+    tie $ebid, 'IPC::Shareable', $ipckey2, {
         create    => 1,
         exclusive => 0,
         mode      => 0644,
         destroy   => 0,
-      };
+    };
 
-    ( tied $ebid )->shlock();
+    (tied $ebid)->shlock();
     my $id = ++$ebid;
-    ( tied $ebid )->shunlock();
+    (tied $ebid)->shunlock();
 
     $id = $name;
     my $fn = $threadprefix . $id;
@@ -56,230 +54,188 @@ sub new {
         Listen => 2000
     ) or die "Couldn't create $fn: $!";
 
-    $sockets{'name'} = $fn;
+    $sockets{name} = $fn;
 
     my $self = {
-        'id'       => $id,
-        'socketfn' => $fn,
-        'socket'   => $server,
-        'name'     => $name,
-        'retire'   => 0,
-        'methods'  => {
-            'test' => \&footest,
+        id       => $id,
+        socketfn => $fn,
+        socket   => $server,
+        name     => $name,
+        retire   => 0,
+        methods  => {
+            test => \&footest,
         },
-        'procedures' => {
-            'test' => \&footest,
+        procedures => {
+            test => \&footest,
         },
-        'self'           => $self_self,
-        'pre_call_trap'  => undef,
-        'post_call_trap' => undef,
+        self           => $self_self,
+        pre_call_trap  => undef,
+        post_call_trap => undef,
     };
     bless $self, $class;
-
-    return $self;
 }
 
 sub AUTOLOAD {
     my $self = shift;
-    my $type = ref($self)
-      or die "$self is not an object";
+    my $type = ref $self or die "$self is not an object";
 
     my $name = $AUTOLOAD;
     $name =~ s/.*://;    # strip fully-qualified portion
 
     return if $name eq 'DESTROY';    # We do not AUTOLOAD destroys
-    return $self->call_thread( $name, @_ );
+    return $self->call_thread($name, @_);
 }
 
 sub call_thread {
-    my $self = shift;
-    my $name = shift;
-    my $args = \@_;
+    my ($self, name, @args) = @_;
 
-    #    print "call_thread(): $self->{name} :: $name:\n" . Dumper( $args ) . "\n\n";
-    #    warn "$self->{name} :: $name Semaphore down\n";
-    #    $self->{'semaphore'}->down();
-    #    warn "\t$self->{name} :: $name Enqueue & clone\n";
+    #print "call_thread(): $self->{name} :: $name:\n" . Dumper(\@args) . "\n\n";
+    #warn "$self->{name} :: $name Semaphore down\n";
+    #$self->{semaphore}->down();
+    #warn "\t$self->{name} :: $name Enqueue & clone\n";
 
-    if ( !defined $self->{'procedures'}->{$name} ) {
-
-        #        (tied $ebid)->shlock();
-        #        $id = ++ $ebid;
-        #        (tied $ebid)->shunlock();
-        #        $recvqueue = msgget( $id, IPC_CREAT | S_IRWXU );
-        #        die "Can't create receiving SysV message queue: $!" if ( !$recvqueue );
+    unless (defined $self->{procedures}{$name}) {
+        #(tied $ebid)->shlock();
+        #$id = ++ $ebid;
+        #(tied $ebid)->shunlock();
+        #$recvqueue = msgget($id, IPC_CREAT | S_IRWXU);
+        #die "Can't create receiving SysV message queue: $!" unless $recvqueue;
     }
 
-    #    (tied $ebid)->shlock();
-    my $frozen_payload = nfreeze( [ $name, $args ] );
+    #(tied $ebid)->shlock();
+    my $frozen_payload = nfreeze([ $name, \@args ]);
 
-    my $client = connect_udx( $self->{'socketfn'} );
-
-    #    my $client = IO::Socket::UNIX->new(Peer  => $self->{ 'socketfn' },
-    #                                Type      => SOCK_STREAM,
-    #                                Timeout   => 10 )     or die $@;
+    my $client = connect_udx($self->{socketfn});
+    #my $client = IO::Socket::UNIX->new(Peer => $self->{socketfn}, Type => SOCK_STREAM, Timeout => 10) or die $@;
     print $client $frozen_payload;
     $client->shutdown(1);
 
-    #    warn "Sent " . length($frozen_payload) . "b\n";
-    #    (tied $ebid)->shunlock();
+    #warn "Sent " . length($frozen_payload) . "b\n";
+    #(tied $ebid)->shunlock();
 
     my $ret;
-    goto call_thread_done if ( $self->{'procedures'}->{$name} );
+    unless ($self->{procedures}{$name}) {
+        #warn "\t$self->{name} :: $name Dequeue reply\n";
+        my $rcvd;
+        while (<$client>) {
+            $rcvd .= $_;
+        }
+        $client->shutdown(2);
+        $ret = thaw($rcvd);
 
-    #    warn "\t$self->{name} :: $name Dequeue reply\n";
-    my $rcvd;
-    while (<$client>) {
-        $rcvd .= $_;
+        #warn Dumper $ret;
+
+        #warn "\t$self->{name} :: $name Semaphore up\n";
+        #$self->{semaphore}->up();
     }
-    $client->shutdown(2);
-    $ret = thaw($rcvd);
 
-    #    warn Dumper( $ret );
-
-    #    warn "\t$self->{name} :: $name Semaphore up\n";
-    #    $self->{'semaphore'}->up();
-
-  call_thread_done:
     $client->shutdown(2);
     $client->close;
 
-    #    warn "$self->{name} :: $name Done\n";
-    return $ret->[0] if defined($ret);
-}
-
-sub set_self {
-    my $self    = shift;
-    my $newself = shift;
-    $self->{'self'} = shift;
+    #warn "$self->{name} :: $name Done\n";
+    return $ret->[0] if defined $ret;
 }
 
 sub run {
-    my $self = shift;
+    my ($self) = @_;
 
     while (1) {
         my $r = $self->oneiteration;
-        return if ( defined($r) and $r == 1 );
+        return if defined $r and $r == 1;
     }
 }
 
 sub oneiteration {
-    my $self = shift;
+    my ($self) = @_;
 
     my $socket;
     my $rcvd;
     try {
-        $socket = $self->{'socket'}->accept();
+        $socket = $self->{socket}->accept();
 
-        #            warn "Accepted $socket";
+        #warn "Accepted $socket";
         while (<$socket>) {
             $rcvd .= $_;
         }
-    }
-    catch {
+    } catch {
         warn "Error reading from socket: $_";
-        return;
     };
 
-    if ( !defined($rcvd) ) {
+    unless (defined $rcvd) {
         warn "Error reading from socket - returning";
         return;
     }
 
     my $message = thaw($rcvd);
-    {
-        if ( scalar( @{$message} ) != 2 ) {
-            print "\n\nW T F: message is not 3:\n" . Dumper($message) . "\n\n";
-            $socket->shutdown(2);
-            $socket->close;
-            next;
-        }
-        my $command = $message->[0];
-        my $args    = $message->[1];
 
-        my $procedure = 0;
-        my $func      = $self->{methods}->{$command};
-        if ( !$func ) {
-            $func      = $self->{procedures}->{$command};
-            $procedure = 1;
-            if ( !$func ) {
-                $socket->shutdown(2);
-                $socket->close;
-                die "No such function or procedure '$command'\n" . Dumper( $self->{'methods'} );
-            }
-        }
-
-        my @arguments;
-        push @arguments, $self->{'self'};
-        if ( ref($args) eq 'ARRAY' ) {
-            foreach my $p ( @{$args} ) {
-                push @arguments, $p;
-            }
-        }
-        else {
-            push @arguments, $args;
-        }
-
-        $self->{'pre_call_trap'}->( \@arguments )
-          if ( $self->{'pre_call_trap'} );
-
-        my $ret = $func->(@arguments);    #@{ $self, $args } );
-
-        #               warn "      Return for $command : " . Dumper( $ret );
-        $self->{'post_call_trap'}->( $self, \@arguments, $ret )
-          if ( $self->{'post_call_trap'} );
-        if ( $procedure != 1 ) {
-            $ret = 0 if !defined($ret);
-            my $frozen_payload = nfreeze( [$ret] );
-            print $socket $frozen_payload;
-
-            #                    $txqueue->enqueue( threads::shared::shared_clone($ret) );
-        }
-
+    if (@{$message} != 2) {
+        say "\n\nW T F: message is not 2:\n", Dumper($message), "\n";
         $socket->shutdown(2);
         $socket->close;
+        next;
+    }
+    my $command = $message->[0];
+    my $args    = $message->[1];
 
-        if ( $self->{retire} == 1 ) {
-            $self->{'socket'}->shutdown(2);
-            $self->{'socket'}->close;
-            return 1;
+    my $procedure = 0;
+    my $func      = $self->{methods}{$command};
+    unless ($func) {
+        $func = $self->{procedures}{$command};
+        $procedure = 1;
+        unless ($func) {
+            $socket->shutdown(2);
+            $socket->close;
+            die "No such function or procedure '$command'\n", Dumper $self->{methods};
         }
+    }
+
+    my @arguments = ($self->{self}, (ref $args eq 'ARRAY') ? @$args : $args);
+
+    $self->{pre_call_trap}->(\@arguments) if $self->{pre_call_trap};
+
+    my $ret = $func->(@arguments);
+
+    #warn "      Return for $command : ", Dumper $ret;
+    $self->{post_call_trap}->($self, \@arguments, $ret) if $self->{post_call_trap};
+    if ($procedure != 1) {
+        $ret = 0 unless defined $ret;
+        my $frozen_payload = nfreeze([$ret]);
+        print $socket $frozen_payload;
+    }
+
+    $socket->shutdown(2);
+    $socket->close;
+
+    if ($self->{retire} == 1) {
+        $self->{socket}->shutdown(2);
+        $self->{socket}->close;
+        return 1;
     }
 }
 
 sub footest {
-    my $self = shift;
-    my ( $a, $b, $c ) = @_;
-    return $a + $b + $c;
+    my ($self, $a, $b, $c) = @_;
+    $a + $b + $c;
 }
 
 sub connect_udx {
-    my $socketfn = shift;
+    my ($socketfn) = @_;
 
     my $client;
-    while (
-        !(
-            $client = IO::Socket::UNIX->new(
-                Peer    => $socketfn,
-                Type    => SOCK_STREAM,
-                Timeout => 10
-            )
-        )
-      ) {
-        if ( $@ =~ /Resource temporarily unavailable/ ) {
+    while ( !($client = IO::Socket::UNIX->new(Peer => $socketfn, Type => SOCK_STREAM, Timeout => 10)) ) {
+        if ($@ =~ /Resource temporarily unavailable/) {
             srand time * $$;
             my $sleepfor = rand(2);
-
-            #            warn "Sleeping for $sleepfor seconds as listen queue is full for $socketfn";
-            select( undef, undef, undef, $sleepfor );
-        }
-        else {
+            #warn "Sleeping for $sleepfor seconds as listen queue is full for $socketfn";
+            select(undef, undef, undef, $sleepfor);
+        } else {
             die "Couldn't connect to '$socketfn': $@";
         }
     }
 
-    return $client;
+    $client;
 }
 
 1;
+
 __END__

lib/Pinscher/Core/EventBus.pm-sysv
@@ -1,295 +1,0 @@
-package Pinscher::Core::EventBus;
-
-use strict;
-use AutoLoader;
-use Data::Dumper;
-use threads;
-use threads::shared;
-use Thread::Queue;
-use Clone qw(clone);
-use Thread::Semaphore;
-use Storable qw(nfreeze thaw);
-use IPC::SysV qw(IPC_PRIVATE IPC_RMID IPC_CREAT S_IRWXU);
-use IPC::Shareable (':lock');
-use POSIX qw(ceil);
-
-our $AUTOLOAD;
-our $ebid = 1234;
-our %sysvqueues;
-our @retqueues;
-
-my $MAXQSEND = 80000;
-
-sub new {
-    my $class     = shift;
-    my $self_self = shift;
-    my $name      = shift;
-
-    tie %sysvqueues, 'IPC::Shareable', 'svmq',
-      {
-        create    => 1,
-        exclusive => 0,
-        mode      => 0644,
-        destroy   => 0,
-      };
-
-    tie $ebid, 'IPC::Shareable', 'idtq',
-      {
-        create    => 1,
-        exclusive => 0,
-        mode      => 0644,
-        destroy   => 0,
-      };
-
-    tie @retqueues, 'IPC::Shareable', 'retv',
-      {
-        create    => 1,
-        exclusive => 0,
-        mode      => 0644,
-        destroy   => 0,
-      };
-
-    ( tied $ebid )->shlock();
-    my $id = ++$ebid;
-    ( tied $ebid )->shunlock();
-    my $queue = msgget( $id, IPC_CREAT | S_IRWXU );
-    warn "id: $id";
-    $sysvqueues{'name'} = $id;
-
-    my $self = {
-        'id'      => $id,
-        'queue'   => $queue,
-        'name'    => $name,
-        'methods' => {
-            'test' => \&footest,
-        },
-        'procedures' => {
-            'test' => \&footest,
-        },
-        'self'           => $self_self,
-        'pre_call_trap'  => undef,
-        'post_call_trap' => undef,
-    };
-    bless $self, $class;
-
-    $self->init_retqueues;
-
-    return $self;
-}
-
-sub AUTOLOAD {
-    my $self = shift;
-    my $type = ref($self)
-      or die "$self is not an object";
-
-    my $name = $AUTOLOAD;
-    $name =~ s/.*://;    # strip fully-qualified portion
-    return $self->call_thread( $name, @_ );
-}
-
-sub init_retqueues {
-    return if ( @retqueues > 0 );
-
-    ( tied @retqueues )->shlock();
-    for ( my $x = 0; $x < 50; $x++ ) {
-        my $id = ++$ebid;
-        my $recvqueue = msgget( $id, IPC_CREAT | S_IRWXU );
-        push @retqueues, $id;
-    }
-    ( tied @retqueues )->shunlock();
-
-}
-
-sub get_retqueue {
-    ( tied @retqueues )->shlock();
-    my $id = pop @retqueues;
-    ( tied @retqueues )->shunlock();
-    my $recvqueue = msgget( $id, IPC_CREAT | S_IRWXU );
-    return ( $id, $recvqueue );
-}
-
-sub return_retqueue {
-    my ( $self, $id ) = @_;
-    ( tied @retqueues )->shlock();
-    push @retqueues, $id;
-    ( tied @retqueues )->shunlock();
-}
-
-sub call_thread {
-    my $self = shift;
-    my $name = shift;
-    my $args = \@_;
-
-    #    print "call_thread(): $self->{name} :: $name:\n" . Dumper( $args ) . "\n\n";
-    #    warn "$self->{name} :: $name Semaphore down\n";
-    #    $self->{'semaphore'}->down();
-    #    warn "\t$self->{name} :: $name Enqueue & clone\n";
-
-    my ( $retid, $id, $retqueue );
-
-    if ( !defined $self->{'procedures'}->{$name} ) {
-        ( $id, $retqueue ) = $self->get_retqueue;
-        $retid = $id;
-
-        #        (tied $ebid)->shlock();
-        #        $id = ++ $ebid;
-        #        (tied $ebid)->shunlock();
-        #        $recvqueue = msgget( $id, IPC_CREAT | S_IRWXU );
-        #        die "Can't create receiving SysV message queue: $!" if ( !$recvqueue );
-    }
-
-    ( tied $ebid )->shlock();
-    my $frozen_payload = nfreeze( [ $name, $args, $id ] );
-
-    my $payload   = pack( "l! a*", 1234, $frozen_payload );
-    my $paylength = length($payload);
-    my $packets   = $paylength / $MAXQSEND;
-    msgsnd( $self->{'queue'}, $packets, 0 ) or warn "Couldn't send number of packets ($packets) to txqueue: $!";
-
-    for ( my $x = 0; $x < $packets; $x++ ) {
-        msgsnd( $self->{'queue'}, substr( $payload, $x * $MAXQSEND, $MAXQSEND ), 0 ) or warn "Couldn't send packet ($x of $packets) to $self->{name} :: $name: $!";
-    }
-    ( tied $ebid )->shunlock();
-
-    my $ret;
-    goto call_thread_done if ( $self->{'procedures'}->{$name} );
-
-    #    warn "\t$self->{name} :: $name Dequeue reply\n";
-    my ( $type_rcvd, $rcvd );
-    warn "Listening on $retqueue / $id";
-    if ( !msgrcv( $retqueue, $rcvd, 1024 * 1024 * 10, 0, 0 ) ) {
-        warn "Couldn't msgrcv!!!";
-    }
-
-    $self->return_retqueue($retid);
-
-    $packets = $rcvd;
-    my $reassembled = '';
-    for ( my $x = 0; $x < $packets; $x++ ) {
-        if ( !msgrcv( $retqueue, $rcvd, 1024 * 1024 * 10, 0, 0 ) ) {
-            warn "Couldn't msgrcv!!!";
-        }
-        my ( $xxx, $yyy ) = unpack( "l! a*", $rcvd );
-
-        $reassembled = pack( "a*a*", $reassembled, $rcvd );
-    }
-
-    warn length($reassembled);
-    ( $type_rcvd, $rcvd ) = unpack( "l! a*", $reassembled );
-    $ret = thaw($rcvd);
-
-    #    warn Dumper( $ret );
-
-    #    warn "\t$self->{name} :: $name Semaphore up\n";
-    #    $self->{'semaphore'}->up();
-
-  call_thread_done:
-
-    #    warn "$self->{name} :: $name Done\n";
-    return $ret->[0] if defined($ret);
-}
-
-sub set_self {
-    my $self    = shift;
-    my $newself = shift;
-    $self->{'self'} = shift;
-}
-
-sub run {
-    my $self = shift;
-
-    while (1) {
-        $self->oneiteration;
-    }
-}
-
-sub oneiteration {
-    my $self = shift;
-
-    my ( $type_rcvd, $rcvd );
-    if ( !msgrcv( $self->{'queue'}, $rcvd, 1024 * 1024, 0, 0 ) ) {
-        warn "Couldn't msgrcv at oneiteration()!!!";
-    }
-
-    my $packets = $rcvd;
-    warn "Receiving $packets in oneiteration";
-    my $reassembled = '';
-    for ( my $x = 0; $x < $packets; $x++ ) {
-        if ( !msgrcv( $self->{'queue'}, $rcvd, 1024 * 1024 * 10, 0, 0 ) ) {
-            warn "Couldn't msgrcv!!!";
-        }
-        $reassembled = pack( "a*a*", $reassembled, $rcvd );
-    }
-
-    ( $type_rcvd, $rcvd ) = unpack( "l! a*", $reassembled );
-    my $message = thaw($rcvd);
-
-    {
-        if ( scalar( @{$message} ) != 3 ) {
-            print "\n\nW T F: message is not 3:\n" . Dumper($message) . "\n\n";
-            next;
-        }
-        my $command = $message->[0];
-        my $args    = $message->[1];
-        my $txqueue = $message->[2];
-
-        my $procedure = 0;
-        my $func      = $self->{methods}->{$command};
-        if ( !$func ) {
-            $func      = $self->{procedures}->{$command};
-            $procedure = 1;
-            if ( !$func ) {
-                die "No such function or procedure '$command'\n" . Dumper( $self->{'methods'} );
-            }
-        }
-
-        my @arguments;
-        push @arguments, $self->{'self'};
-        if ( ref($args) eq 'ARRAY' ) {
-            foreach my $p ( @{$args} ) {
-                push @arguments, $p;
-            }
-        }
-        else {
-            push @arguments, $args;
-        }
-
-        $self->{'pre_call_trap'}->( \@arguments )
-          if ( $self->{'pre_call_trap'} );
-
-        my $ret = $func->(@arguments);    #@{ $self, $args } );
-
-        #               warn "      Return for $command : " . Dumper( $ret );
-        $self->{'post_call_trap'}->( $self, \@arguments, $ret )
-          if ( $self->{'post_call_trap'} );
-        if ( $procedure != 1 ) {
-            $ret = 0 if !defined($ret);
-            my $frozen_payload = nfreeze( [$ret] );
-            my $sysvtxqueue = msgget( $txqueue, S_IRWXU );
-            my $payload = pack( "l! a*", 1234, $frozen_payload );
-            my $paylength = length($payload);
-
-            my $packets = $paylength / $MAXQSEND;
-            msgsnd( $sysvtxqueue, $packets, 0 ) or warn "Couldn't send number of packets ($packets) to txqueue: $!";
-
-            warn "Sending to $sysvtxqueue";
-
-            for ( my $x = 0; $x < $packets; $x++ ) {
-                my $packet = substr( $payload, $x * $MAXQSEND, $MAXQSEND );
-                $packet = pack( "l! a*", 1234, $packet );
-                warn Dumper( [ $sysvtxqueue, length($packet), 0 ] );
-                if ( !msgsnd( $sysvtxqueue, $packet, 0 ) ) { die "Couldn't send packet ($x of $packets) to txqueue ($sysvtxqueue): $!"; }
-            }
-
-            #                    $txqueue->enqueue( threads::shared::shared_clone($ret) );
-        }
-    }
-}
-
-sub footest {
-    my $self = shift;
-    my ( $a, $b, $c ) = @_;
-    return $a + $b + $c;
-}
-
-1;

commit 4b1a9008f9ab5bda9dbfe40b09bf2ad7dd8b07ea

fixed WorkerThread which i broke way back in b956a7c

lib/Synacor/Disbatch/WorkerThread.pm
@@ -13,7 +13,6 @@ sub new {
     my ($class, $id, $queue) = @_;
 
     my $self = {
-        eb        => Pinscher::Core::EventBus->new(bless({}, $class), "worker#$id"),
         id        => $id,
         queue     => $queue,
         config    => $Synacor::Disbatch::Engine::Engine->{config},
@@ -21,6 +20,7 @@ sub new {
         tasks_run => 0,
     };
 
+    $self->{eb} = Pinscher::Core::EventBus->new($self, "worker#$id");
     $self->{eb}{procedures}{start_task} = \&start_task;
     $self->{eb}{methods}{retire}        = \&retire;
 