mbusigin/disbatch

commit e38b6f68e9a6848f9895b18771e8b2840cfeec7e

*** BREAKING CHANGE: kill AUTOLOAD in EventBus, using call_thread() directly instead
In plugins, this needs to be changed:

  $Synacor::Disbatch::Engine::EventBus->report_task_done(@args)

to:

  $Synacor::Disbatch::Engine::EventBus->call_thread('report_task_done', @args)

lib/Synacor/Disbatch/HTTP.pm
@@ -184,7 +184,7 @@ sub set_queue_attr_json {
     my $queueid = $cgi->param('queueid');
     return {success => 0, error => 'You must supply a queueid'} unless $queueid;
 
-    my ($r, $e) = @{$Synacor::Disbatch::Engine::EventBus->set_queue_attr($queueid, $attr, $value)};
+    my ($r, $e) = @{$Synacor::Disbatch::Engine::EventBus->call_thread('set_queue_attr', $queueid, $attr, $value)};
     my $ret = {success => $r};
     $ret->{error} = $e if defined $e;
     $ret;

bin/disbatchd.pl
@@ -132,9 +132,9 @@ $engine->logger->trace('Loading queues');
 $engine->load_queues;
 $engine->logger->trace('Loaded queues, starting schedulers');
 
-my $timer = Synacor::Disbatch::Timer->new($config->{schedulertimerinterval}, sub { $engine->{eb}->awaken });
+my $timer = Synacor::Disbatch::Timer->new($config->{schedulertimerinterval}, sub { $engine->{eb}->call_thread('awaken') });
 
-my $timer2 = Synacor::Disbatch::Timer->new($config->{updatetimerinterval}, sub { $engine->{eb}->update_node_status });
+my $timer2 = Synacor::Disbatch::Timer->new($config->{updatetimerinterval}, sub { $engine->{eb}->call_thread('update_node_status') });
 
 $engine->logger->trace('Spinning up HTTP service');
 my $http = Synacor::Disbatch::HTTP->new($config->{httpport});

lib/Pinscher/Core/EventBus.pm
@@ -3,7 +3,6 @@ package Pinscher::Core::EventBus;
 use 5.12.0;
 use warnings;
 
-use AutoLoader;
 use Data::Dumper;
 use File::Temp qw/ tempfile tempdir /;
 use IO::Socket qw(SOCK_STREAM);
@@ -16,9 +15,6 @@ use Try::Tiny;
 
 # our because disbatchd.pl will set these if defined in disbatch.ini
 our $threadprefix = "/tmp/thread_";
-
-# our because otherwise it won't work
-our $AUTOLOAD;
 
 sub new {
     my ($class, $self_self, $id) = @_;
@@ -50,25 +46,12 @@ sub new {
     bless $self, $class;
 }
 
-sub AUTOLOAD {
-    my $self = shift;
-    my $type = ref $self or die "$self is not an object";
-
-    my $name = $AUTOLOAD;
-    $name =~ s/.*://;    # strip fully-qualified portion
-
-    return if $name eq 'DESTROY';    # We do not AUTOLOAD destroys
-    return $self->call_thread($name, @_);
-}
-
-# used once in AUTOLOAD()
+# used to be used once in AUTOLOAD(), now called directly by what needs it
 sub call_thread {
     my ($self, $name, @args) = @_;
 
     my $frozen_payload = nfreeze([ $name, \@args ]);
-
     my $client = connect_udx($self->{socketfn});
-
     print $client $frozen_payload;
     $client->shutdown(1);
 

lib/Synacor/Disbatch/Queue.pm
@@ -68,7 +68,7 @@ sub schedule {
         my $thr = $self->get_free_thread();
         $self->{threads_inuse}{$task->{id}} = $thr;
         $self->wtfer->trace("schedule starting actual task: $document->{_id}");
-        $thr->{eb}->start_task($task);
+        $thr->{eb}->call_thread('start_task', $task);
         push @{$self->{tasks_doing}}, $task;
     }
     1;
@@ -130,7 +130,7 @@ sub report_task_done {
             $thr->{tasks_run}++;
             if ($self->{config}{tasks_before_workerthread_retirement} != 0 and $thr->{tasks_run} >= $self->{config}{tasks_before_workerthread_retirement}) {
                 $self->logger->info("Worker thread is retiring after $thr->{tasks_run} tasks");
-                $self->{threads_inuse}{$taskid}{eb}->retire;
+                $self->{threads_inuse}{$taskid}{eb}->call_thread('retire');
                 POSIX::close($self->{threads_inuse}{$taskid}{eb}{socket}->fileno);
                 delete $self->{threads_inuse}{$taskid};
                 $self->start_thread_pool;

lib/Synacor/Disbatch/WorkerThread.pm
@@ -48,7 +48,7 @@ sub start_task {
     }
     catch {
         $self->logger("Thread has uncaught exception: $_");
-        $Synacor::Disbatch::Engine::EventBus->report_task_done($task->{queue_id}, $task->{_id}, 2, 'Unable to complete', "Thread has uncaught exception: $_");
+        $Synacor::Disbatch::Engine::EventBus->call_thread('report_task_done', $task->{queue_id}, $task->{_id}, 2, 'Unable to complete', "Thread has uncaught exception: $_");
     };
 
     1;

commit 18167bdda3d8cee845ebea1449959c17edfb4e0d

fix lib/Synacor/Disbatch/HTTP.pm broken by changes in e38b6f6

lib/Synacor/Disbatch/HTTP.pm
@@ -167,7 +167,7 @@ sub kill {
 sub scheduler_json {
 
     #my ($cgi) = @_;
-    $Synacor::Disbatch::Engine::EventBus->scheduler_report;
+    $Synacor::Disbatch::Engine::EventBus->call_thread('scheduler_report');
 }
 
 sub set_queue_attr_json {
@@ -192,12 +192,12 @@ sub set_queue_attr_json {
 
 sub list_users_json {
     my ($cgi) = @_;
-    $Synacor::Disbatch::Engine::EventBus->list_users($cgi->param('group'), $cgi->param('filter'));
+    $Synacor::Disbatch::Engine::EventBus->call_thread('list_users', $cgi->param('group'), $cgi->param('filter'));
 }
 
 sub start_queue_json {
     my ($cgi) = @_;
-    $Synacor::Disbatch::Engine::EventBus->construct_queue($cgi->param('type'), $cgi->param('name'));
+    $Synacor::Disbatch::Engine::EventBus->call_thread('construct_queue', $cgi->param('type'), $cgi->param('name'));
 }
 
 sub queue_create_tasks_json {
@@ -205,34 +205,33 @@ sub queue_create_tasks_json {
 
     my $jsobj = $cgi->param('object');
     my $obj   = $json->decode($jsobj);
-    $Synacor::Disbatch::Engine::EventBus->queue_create_tasks($cgi->param('queueid'), $obj);
+    $Synacor::Disbatch::Engine::EventBus->call_thread('queue_create_tasks', $cgi->param('queueid'), $obj);
 }
 
 sub queue_create_tasks_from_query_json {
     my ($cgi) = @_;
     my $filter = ($cgi->param('jsonfilter')) ? $json->decode($cgi->param('jsonfilter')) : $cgi->param('filter');
-    $Synacor::Disbatch::Engine::EventBus->queue_create_tasks_from_query($cgi->param('queueid'), $cgi->param('collection'), $filter, $json->decode($cgi->param('parameters')));
+    $Synacor::Disbatch::Engine::EventBus->call_thread('queue_create_tasks_from_query', $cgi->param('queueid'), $cgi->param('collection'), $filter, $json->decode($cgi->param('parameters')));
 }
 
 sub queue_prototypes_json {
-
     #my ($cgi) = @_;
-    $Synacor::Disbatch::Engine::EventBus->queue_prototypes;
+    $Synacor::Disbatch::Engine::EventBus->call_thread('queue_prototypes');
 }
 
 sub reload_queues_json {
-    $Synacor::Disbatch::Engine::EventBus->reload_queues;
+    $Synacor::Disbatch::Engine::EventBus->call_thread('reload_queues');
     [1];
 }
 
 sub search_tasks_json {
     my ($cgi) = @_;
-    $Synacor::Disbatch::Engine::EventBus->search_tasks($cgi->param('queue'), $cgi->param('filter'), $cgi->param('json') || 0, $cgi->param('limit') || 0, $cgi->param('skip') || 0, $cgi->param('count') || 0, $cgi->param('terse') || 0);
+    $Synacor::Disbatch::Engine::EventBus->call_thread('search_tasks', $cgi->param('queue'), $cgi->param('filter'), $cgi->param('json') || 0, $cgi->param('limit') || 0, $cgi->param('skip') || 0, $cgi->param('count') || 0, $cgi->param('terse') || 0);
 }
 
 sub delete_queue_json {
     my ($cgi) = @_;
-    {success => $Synacor::Disbatch::Engine::EventBus->delete_queue($cgi->param('id'))};
+    {success => $Synacor::Disbatch::Engine::EventBus->call_thread('delete_queue', $cgi->param('id'))};
 }
 
 sub logger {