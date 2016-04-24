fd3916fcfc542f34501309437da9372635076836
Ashley Willis <ashleyw@cpan.org>
2016-04-23 22:14:34 -0400

    have lib/Disbatch/Plugin/Demo.pm use new way, but still support old way

--- lib/Disbatch/Plugin/Demo.pm
+++ lib/Disbatch/Plugin/Demo.pm
@@ -7,12 +7,30 @@ use boolean;
 use Data::Dumper;
 
 sub new {
-    my ($class, $queue, $parameters) = @_;
+    my $class = shift;
 
-    warn Dumper $parameters;
-    my %self = map { $_ => $parameters->{$_} } keys %$parameters;       # modifying $parameters breaks something.
-    $self{queue_id} = $queue->{id};
-    bless \%self, $class;
+    # deprecated Disbatch 3 format
+    if (ref $_[0]) {
+        my ($queue, $parameters) = @_;
+        warn Dumper $parameters;
+        my %self = map { $_ => $parameters->{$_} } keys %$parameters;       # modifying $parameters breaks something in Disbatch 3.
+        $self{queue_id} = $queue->{id};
+        return bless \%self, $class;
+    }
+
+    my $self = { @_ };
+    warn Dumper $self->{task}{parameters};
+
+    # back-compat, so as to not change Disbatch 3 plugins so much
+    # stick all params in $self
+    for my $param (keys %{$self->{task}{parameters}}) {
+        next if $param eq 'workerthread' or $param eq 'task';
+        $self->{$param} = $self->{task}{parameters}{$param};
+    }
+    $self->{queue_id} = $self->{task}{queue};
+    $self->{id} = $self->{task}{_id};
+
+    bless $self, $class;
 }
 
 sub run {
@@ -77,24 +95,40 @@ Disbatch::Plugin::Demo - demo plugin for Disbatch
 
 =head1 DESCRIPTION
 
-Has commands 'a' (stdout, success), 'b' (stderr, success), and 'c' (stderr, failure)
+A sample Disbatch plugin.
+
+Tasks for this plugin should have in C<parameters> the name C<commands> with a value of C<a>, C<b>, C<c>, or any combination, and optionally the name C<counter>.
+Any other characters in the C<commands> value are ignored, as well as any other names in C<parameters>.
+
+Command C<a> will write to C<stdout> and succeed with status 1.
+
+Command C<b> will write to C<stderr> and succeed with status 1.
+
+Command C<c> will write to C<stderr> and fail with status 2.
 
 =head1 SUBROUTINES
 
 =over 2
 
+=item new(workerthread => $disbatch, task => $doc);
+
+Parameters: C<<$disbatch>> is the C<Disbatch> object from C<task_runner>, C<$doc> is the task document from MongoDB.
+
+Returns a C<Disbatch::Plugin::Demo> object.
+
+In this demo, the parameters passed become C<$self>, and all of the task's parameters are put into C<$self>, unless they are named C<workerthread> or C<task>.
+In addition, C<<$self->{queue_id}>> is set to the task's queue id, and C<<$self->{id}>> is set to the task's id.
+This allows minimal modification to Disbatch 3 plugins.
+
 =item new($queue, $parameters)
+
+I<DEPRECATED FORMAT> for usage with Disbatch 3.
 
 Parameters: C<< { id => $oid } >> where C<$id> is a C<MongoDB::OID> object of the task's queue value, C<HASH> parameters value of the task.
 
 Returns a C<Disbatch::Plugin::Demo> object.
-In this demo, parameters is a C<HASH> with the key C<commands> and a value of C<a>, C<b>, C<c>, or any combination; and optionally the key C<counter>.
-This example will overwrite the parameter C<queue_id> if used.
-Any other characters in the C<commands> value are ignored, as well as any other keys.
-Parameters does not have to be a C<HASH> in your plugin – it can be any value MongoDB can store.
 
-The L<task_runner> command will overwrite any parameter with key name C<id> or C<workerthread>,
-as these are used to store the C<MongoDB::OID> object of the task and the C<Disbatch> object, respectively.
+In this demo, the task's parameters become C<$self>, and C<<$self->{queue_id}>> is set to C<<$queue->{id}>>.
 
 =item run
 
